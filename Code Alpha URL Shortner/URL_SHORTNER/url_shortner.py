226e2ae4e0497e83a74c3b15cb8126842fd703dc
Vaibhavi Patle <ansupatle4@gmail.com>
2024-08-23 20:51:33 +0530

    Add files via upload

---
+++ Code Alpha URL Shortner/URL_SHORTNER/url_shortner.py
@@ -0,0 +1,14 @@
+from flask import Flask,request,render_template
+import pyshorteners
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    if request.method== 'GET':
+        url= request.args['name']
+        s=pyshorteners.Shortener()
+        shorter=s.tinyurl.short(url)
+        return render_template('home.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
